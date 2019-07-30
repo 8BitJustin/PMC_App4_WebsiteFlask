e1620fc5898ce7de67f495aaf4f40dcaefcb8658
8bitjustin <j.olson.digital@gmail.com>
2019-07-30 07:37:30 -0700

    Added about section to file.

--- script1.py
+++ script1.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Content goes here!"
+    return "Home page"
+
+
+@app.route('/about/')
+def about():
+    return "About page"
 
 
 if __name__ == "__main__":
